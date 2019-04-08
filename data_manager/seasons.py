f1d848c381e1072a754e54b5c3ef1e3f340102fe
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-08 10:32:29 +0200

    validation added

--- data_manager/seasons.py
+++ data_manager/seasons.py
@@ -1,2 +1,8 @@
 from data import queries
 
+
+def check_data(season_id):
+    try:
+        return queries.get_season_details(season_id)
+    except Exception as error:
+        raise error
--- data_manager/shows.py
+++ data_manager/shows.py
@@ -1,6 +1,14 @@
 from data import queries
 
 
+def check_shows():
+    try:
+        shows = queries.get_shows_for_table()
+        return shows
+    except Exception as error:
+        return error
+
+
 def check_data(show_id):
     try:
         return queries.get_show_details(show_id)
