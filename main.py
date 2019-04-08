d1cd994e49afe59601865cbfa12cbfbb1fb9c93b
Michal Kalinowski <subaru.kali@gmail.com>
2019-04-08 10:33:22 +0200

    route for table added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, jsonify
 from data import queries
 from data_manager import shows, seasons
 
@@ -11,15 +11,27 @@ def index():
     return render_template('index.html', shows=shows)
 
 
+@app.route('/tv-show/<show_id>')
+def show_details(show_id):
+    show = queries.get_show_details(show_id)
+    return render_template('show_details.html', show=show)
+
+
 @app.route('/design')
 def design():
     return render_template('design.html')
 
 
-@app.route('/tv-show/<show_id>')
-def show_details(show_id):
-    show = shows.check_data(show_id)
-    return render_template('show_details.html', show=show)
+@app.route('/table', methods=['GET','POST'])
+def json_table():
+    shows_table = shows.check_shows()
+    return jsonify(shows_table)
+
+
+@app.route('/tv-show/<show_id>/<season_id>')
+def show_season_details(show_id, season_id):
+
+    pass
 
 
 def main():
